6bd06fb21396f3138af1fd8711354dabf8f5c5e3
MakerSquare Student <students@themakersquare.com>
2013-09-30 10:45:46 -0500

    Add test for wardrobe item controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LionWitch::Application.routes.draw do
+  resources :wardrobe_items
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
